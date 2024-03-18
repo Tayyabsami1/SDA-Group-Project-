44599e814fc22ecffcd1917bb17dea17417a6ca0
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2024-03-18 10:32:46 +0500

    Proper Folder structure

--- WeatherApp/src/main/java/module-info.java
+++ WeatherApp/src/main/java/module-info.java
@@ -1,13 +1,8 @@
 module org.example.weatherapp {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
 
     opens org.example.weatherapp to javafx.fxml;
     exports org.example.weatherapp;
