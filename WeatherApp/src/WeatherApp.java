c8877d92f1be0ded33f51e41c2782683ad3634d2
uzairahmad12389 <125276051+uzairahmad12389@users.noreply.github.com>
2024-03-18 21:49:51 +0500

    Update WeatherApp.java

---
+++ WeatherApp/src/WeatherApp.java
@@ -0,0 +1,40 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+// retreive weather data from API - this backend logic will fetch the latest weather
+// data from the external API and return it. The GUI will
+// display this data to the user
+public class WeatherApp {
+    // fetch weather data for given location
+    public static JSONObject getWeatherData(String locationName){
+        // get location coordinates using the geolocation API
+        JSONArray locationData = getLocationData(locationName);
+
+        // extract latitude and longitude data
+        JSONObject location = (JSONObject) locationData.get(0);
+        double latitude = (double) location.get("latitude");
+        double longitude = (double) location.get("longitude");
+
+        // build API request URL with location coordinates
+        String urlString = "https://api.open-meteo.com/v1/forecast?" +
+                "latitude=" + latitude + "&longitude=" + longitude +
+                "&hourly=temperature_2m,relativehumidity_2m,weathercode,windspeed_10m&timezone=America%2FLos_Angeles";
+
+        try{
+            // call api and get response
+            HttpURLConnection conn = fetchApiResponse(urlString);
+
+            // check for response status
+            // 200 - means that the connection was a success
+            if(conn.getResponseCode() != 200){
+                System.out.println("Error: Could not connect to API");
+                return null;
+            }
