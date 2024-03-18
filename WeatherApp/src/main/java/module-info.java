62d2eddd210bf0ef2672a637afa1647c96c3715b
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2024-03-18 20:37:15 +0500

    Added the Gui logic

--- WeatherApp/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.weatherapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens org.example.weatherapp to javafx.fxml;
-    exports org.example.weatherapp;
-}
